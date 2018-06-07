486949616992dcfb844c7f45f957bd71703a03eb
Ravivarshney <40038987+Ravivarshney@users.noreply.github.com>
2018-06-07 12:10:09 -0700

    Add files via upload

---
+++ largest_no_formed.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+int n,a[10000],i,j,s,c;
+scanf("%d",&n);
+if (n>0)
+{
+for (i=1;i<=n;i++)
+{scanf("%d",&a[i]);}
+for (i=1;i<n;i++)
+{for (j=i+1;j<n;j++)
+
+{
+    if(a[j-1]>a[j])
+			{
+				s=a[j-1];
+				a[j-1]=a[j];
+				a[j]=s;
+
+			}
+
+			}
+			}
+
+    for(i=n-1;i>0;i--)
+    printf("%d",a[i]);
+
+    }
+
+    }
